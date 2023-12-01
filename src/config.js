e055055717db73cd9da48e20a5993d3e204eeaf5
G010604 <127454526+G010604@users.noreply.github.com>
2023-12-01 11:09:32 -0300

    Delete src directory

--- src/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || 'seuSegredoJWT',
-    mongoURI: process.env.MONGO_URI || 'mongodb+srv://guyuusuke:twq22222@videogames.owmvyts.mongodb.net/?retryWrites=true&w=majority',
-  };
--- src/middleware/errorMiddleware.js
+++
@@ -1,7 +0,0 @@
-const errorMiddleware = (err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ error: 'Erro interno do servidor' });
-  };
-  
-  module.exports = errorMiddleware;
-  
