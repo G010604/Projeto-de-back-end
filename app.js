eeba41f04d2db251e3af2b88b067d15dcb2c6cd9
G010604 <127454526+G010604@users.noreply.github.com>
2023-12-04 12:50:50 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const jogosRoutes = require('./routes/jogos.js');
 const loginRoutes = require('./routes/login.js');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./docs.json');
 
 const app = express();
 
 app.use(express.json());
 
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/jogos', jogosRoutes);
 app.use('/', loginRoutes);
 
+
 app.listen(3000, () => {
-    mongoose.connect(process.env.MONGO_KEY_ACCESS)
-    console.log(`App running...`);
+    mongoose.connect(process.env.MONGO_KEY_ACCESS);
+    console.log('App running...');
 });
